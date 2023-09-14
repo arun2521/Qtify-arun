6dda264a1c6e0a6c41bbaf9b26d8063becc7b147
arun2521 <arunlee810@gmail.com>
2023-09-14 22:13:55 +0530

    Hero and Card sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Hero from './Components/Hero/Hero';
 import NavBar from './Components/NavBar/NavBar';
+import { fetchTopAlbums } from './api/api';
+import Card from './Components/Card/Card';
 
 function App() {
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+
+  const generateData = async () => {
+    try {
+      const data = await fetchTopAlbums();
+      setTopAlbumsData(data);
+    } catch(err) {
+      console.err(err);
+    }
+  }
+
+  useEffect(() => {
+    generateData();
+  },[])
+
   return (
     <div className="App">
       <NavBar/>
+      <Hero />
+      {topAlbumsData.map((item) => {
+        return (
+          <Card key={item.id} data={item} type="album"/>
+        )
+      })}
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import HeroImage from '../../assets/heroImage.svg';
+import styles from './Hero.module.css';
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+        <div>
+            <h1>100 Thousand Songs, ad-free</h1>
+            <h1>Over thousands podcast episodes</h1>
+        </div>
+        <div>
+            <img src={HeroImage} width={212} alt="headphones" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
